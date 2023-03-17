1f6614a5eb128a4ce77aaa7d98df466a75c812e4
@julien.roberge <julien1.roberge@gmail.com>
2023-03-17 17:45:02 +0100

    dog

--- app/src/main/java/com/example/graphicman/GameView.java
+++ app/src/main/java/com/example/graphicman/GameView.java
@@ -32,6 +32,7 @@ public class GameView extends SurfaceView implements SurfaceHolder.Callback{
     private int iJeuxEnCour = 0;
     private TouchButton touchButton;
     private TouchButton donttouchButton;
+    private PetThedog petTheDog;
     private String AUDIOPATH;
     private Context context;
     private int score;
@@ -70,7 +71,8 @@ public class GameView extends SurfaceView implements SurfaceHolder.Callback{
         jeuxPossibles.add(new Bougies(this, AUDIOPATH, true));
         jeuxPossibles.add(new Bougies(this, AUDIOPATH, false));
         jeuxPossibles.add(new GameBoy(context, sensorManager,this, screenWidth, screenHeight));
-
+        petTheDog = new PetThedog(context, this, screenWidth, screenHeight);
+        jeuxPossibles.add(petTheDog);
         nextJeu();
     }
 
@@ -147,8 +149,10 @@ public class GameView extends SurfaceView implements SurfaceHolder.Callback{
     public void onTouch(MotionEvent motionEvent) {
         touchButton.buttonTouch(motionEvent);
         donttouchButton.buttonTouch(motionEvent);
+
     }
 
     public void onDrag(MotionEvent motionEvent) {
+        petTheDog.drag(motionEvent);
     }
 }
---
+++ app/src/main/java/com/example/graphicman/PetThedog.java
@@ -0,0 +1,95 @@
+package com.example.graphicman;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.drawable.Drawable;
+import android.view.MotionEvent;
+
+public class PetThedog extends Jeu {
+    private GameView gameView;
+    private CanvasWrapper canvasWrapper;
+    private Chrono chrono;
+
+    private Drawable sadDog;
+    private Drawable hapyDog1;
+    private Drawable hapyDog2;
+
+    private int pet = 0;
+    private int frame = 0;
+
+    private boolean isPeted = false;
+    private boolean hasBeenPeted = false;
+
+    public PetThedog(Context context, GameView gameView, int width, int height){
+        this.gameView = gameView;
+        this.canvasWrapper = new CanvasWrapper(width, height);
+        this.chrono = new Chrono();
+
+        this.sadDog= context.getDrawable(R.drawable.saddog);
+        this.hapyDog1 = context.getDrawable(R.drawable.hapydog1);
+        this.hapyDog2 = context.getDrawable(R.drawable.happidog2);
+    }
+
+    public void drag(MotionEvent motionEvent) {
+        pet++;
+        if (pet>50){
+            isPeted = false;
+            hasBeenPeted = false;
+            pet = 0;
+            gameView.nextJeu();
+        }
+        isPeted = true;
+    }
+
+    public void drawSadDog(int x, int y){
+        canvasWrapper.drawImage(sadDog,x,y,x+800,y+800);
+    }
+
+    public void boucleHappyDog(int x, int y){
+        switch (frame) {
+            case 0:
+                canvasWrapper.drawImage(hapyDog1, x, y, x + 800, y + 800);
+                frame++;
+                break;
+            case 1:
+                canvasWrapper.drawImage(hapyDog2, x, y, x + 800, y + 800);
+                frame = 0;
+                break;
+            case 2:
+        }
+    }
+
+    @Override
+    public void draw(Canvas canvas) {
+        canvasWrapper.setCanvas(canvas);
+        int x = 0;
+        int y = 0;
+        if(isPeted){
+            boucleHappyDog(x, y);
+        }else{
+            drawSadDog(x,y);
+        }
+
+    }
+
+    @Override
+    public void update() {
+        if (chrono.isFinit() && !hasBeenPeted){
+            gameView.perdu("il fallait caresser le chien");
+        }else if(chrono.isFinit() && hasBeenPeted){
+           hasBeenPeted = false;
+            isPeted = false;
+            pet = 0;
+           gameView.nextJeu();
+        }
+    }
+
+    @Override
+    public void start() {
+        isPeted = false;
+        hasBeenPeted = false;
+        pet = 0;
+        chrono.start(5000);
+        frame = 0;
+    }
+}
