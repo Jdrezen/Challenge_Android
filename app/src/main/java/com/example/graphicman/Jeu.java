5fe76b170fad1a34439f93f9d361a25ffda00e6d
@julien.roberge <julien1.roberge@gmail.com>
2023-03-17 09:59:49 +0100

    game view

--- app/src/main/java/com/example/graphicman/Jeu.java
+++ app/src/main/java/com/example/graphicman/Jeu.java
@@ -1,8 +1,10 @@
 package com.example.graphicman;
 
+import android.graphics.Canvas;
+
 public abstract class Jeu {
     private int time = 5;
-    public abstract void draw();
+    public abstract void draw(Canvas canvas);
     public abstract void update();
     public abstract void start();
 }
